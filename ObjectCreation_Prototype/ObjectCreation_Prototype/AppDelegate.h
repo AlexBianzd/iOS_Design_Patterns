2ecb1938aa356c40124311edbc10a47104a61b44
Alex <bianzhendong@outlook.com>
2016-12-11 00:00:06 +0800

    ObjectCreation_Prototype

---
+++ ObjectCreation_Prototype/ObjectCreation_Prototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectCreation_Prototype
+//
+//  Created by AlexBian on 10/12/2016.
+//  Copyright © 2016 Alex Bian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectCreation_Prototype/ObjectCreation_Prototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectCreation_Prototype
+//
+//  Created by AlexBian on 10/12/2016.
+//  Copyright © 2016 Alex Bian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
